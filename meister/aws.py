7cdef10e484c8c33e2503e115dc044f7d40f1a34
Fabian Sorqvist <fabian@sorqvist.nu>
2012-11-18 17:28:33 +0100

    Implement security groups

--- meister/aws.py
+++ meister/aws.py
@@ -1,5 +1,7 @@
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
+from libcloud.utils.xml import fixxpath, findtext, findattr, findall
+from libcloud.compute.drivers.ec2 import NAMESPACE 
 
 class AWSConnection:
     """
@@ -10,27 +12,84 @@ class AWSConnection:
     def __init__(self, driver, ec2_id, ec2_key):
         Driver = get_driver(driver)
         self.conn = Driver(ec2_id, ec2_key)
+        self.securityGroups = None
+        self.nodes = None
 
     def getNodes(self):
         """
         Get a list of nodes.
         """
         if not self.nodes:
-            self.nodes = self.conn.list_nodes()
-            return self.nodes
+            self.nodes = self.getDict(self.conn.list_nodes(), 'name')
+        return self.nodes
 
     def createSecurityGroup(self, name, description):
         """
         Create a security groups
         """
         self.conn.ex_create_security_group(name, description)
-        return self
+        group = AWSSecurityGroup(self.conn, name, description)
+        return group
+    
+    def getSecurityGroups(self):
+        """
+        Get a list of all security groups.
+        """
+        if not self.securityGroups:
+            params = {
+                'Action': 'DescribeSecurityGroups',
+            }
+            self.securityGroups = self._to_securityGroups(self.conn.connection.request(self.conn.path, params=params).object)
+        return self.securityGroups
 
-    def addSecurityRule(self, name, fromPort, toPort, ip, protocol='tcp'):
-        self.conn.ex_authorize_security_group(name, fromPort, ip, protocol)
-        return self
+    def deleteSecurityGroup(self, name):
+        """
+        Delete a security group.
+        @param name The name of the parameter.
+        """
+        if isinstance(name, AWSSecurityGroup):
+            name = name.name
 
-    def createNode(self, image_id, size_id, name, size='8'):
+        params = {
+         'Action': 'DeleteSecurityGroup',
+         'GroupName': name,
+        }
+        if self.securityGroups and name in self.securityGroups:
+            del self.securityGroups[name]
+        self.conn.connection.request(self.conn.path, params=params)
+    
+    def _to_securityGroups(self, object):
+        """
+        Convert a list from aws to security group objects.
+        """
+        groups = {}
+        for el in object.findall(fixxpath(xpath='securityGroupInfo/item', namespace=NAMESPACE)):
+            group = self._to_securityGroup(el)
+            groups[group.name] = group
+        return groups
+
+    def _to_securityGroup(self, element):
+        """
+        Convert a SecurityGroupInfo aws object to a python object.
+        """
+        group = AWSSecurityGroup(
+            self.conn,    
+            findtext(element=element, xpath='groupName',
+                          namespace=NAMESPACE),
+            findtext(element=element, xpath='groupDescription',
+                          namespace=NAMESPACE),
+        )
+        for ipRule in element.findall(fixxpath(xpath='ipPermissions/item', namespace=NAMESPACE)):
+            group.addRule(
+                          findtext(element=ipRule, xpath="fromPort", namespace=NAMESPACE),
+                          findtext(element=ipRule, xpath="toPort", namespace=NAMESPACE) ,
+                          findtext(element=ipRule, xpath="ipRanges/item/cidrIp", namespace=NAMESPACE),
+                          findtext(element=ipRule, xpath="ipProtocol", namespace=NAMESPACE) ,
+                          False
+                          )
+        return group
+
+    def createNode(self, image_id, size_id, name, size='8', securityGroup=None, zone=None):
         """
         Create a node on aws.
         """
@@ -41,8 +100,11 @@ class AWSConnection:
          'MaxCount': '1',
          'InstanceType': size_id,
          'BlockDeviceMapping.0.DeviceName': '/dev/sda1',
-         'BlockDeviceMapping.0.Ebs.VolumeSize': size
+         'BlockDeviceMapping.0.Ebs.VolumeSize': size,
+         
         }
+        if securityGroup:
+            params['SecurityGroup.0'] = securityGroup
         object = self.conn.connection.request(self.conn.path, params=params).object
         nodes = self.conn._to_nodes(object, 'instancesSet/item')
         for node in nodes:
@@ -63,12 +125,46 @@ class AWSConnection:
         >>> aws.destroyNode(node.id)
         >>> node in node.getNodes()
         """
-        nodes = self.getDict(self.getNodes())
+        nodes = self.getNodes()
         if nodes[node_id]:
             return self.conn.destroy_node(nodes[node_id])
       
-    def getDict(self, libcloudlist):
+    def getDict(self, libcloudlist, property="id"):
         cloudDict = {}
         for item in libcloudlist:
-            cloudDict[item.id] = item
+            cloudDict[getattr(item, property)] = item
         return cloudDict
+    
+class AWSSecurityGroup:
+    """
+    A security group
+    """
+    def __init__(self, con, name, description):
+        self.con = con
+        self.name = name
+        self.description = description
+        self.rules = []
+    
+    def addRule(self, fromPort, toPort, ip, protocol='tcp', commit=True):
+        """
+        Add a security rule to a group.
+        """
+        rule = {
+            'fromPort': fromPort,
+            'toPort': toPort,
+            'ip': ip,
+            'protocol': protocol
+        }
+        if not rule in self.rules:
+            if commit:
+                self.con.ex_authorize_security_group(self.name, fromPort, toPort, ip, protocol)
+            self.rules.append(rule)
+        return self
+    
+ 
+    def listRules(self):
+        """
+        List all available rules.
+        """
+        return self.rules
+    
