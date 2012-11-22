2c11b3fec72f4b073294bf2f48901f58976ce472
Fabian Sorqvist <fabian@sorqvist.nu>
2012-11-22 21:05:03 +0100

    Remove unused test py file

--- test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import meister.test.aws
-
-def main():
-    print "Running tests"
-    suite = unittest.TestLoader().loadTestsFromTestCase(meister.test.aws.AWSConnectionTest)
-    unittest.TextTestRunner(verbosity=2).run(suite)
-
-main()
