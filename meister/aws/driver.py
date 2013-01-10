b790aa9bbf3301cb9c349bec6a806ea96b7461b1
Fabian Sorqvist <fabian@sorqvist.nu>
2013-01-10 16:28:43 +0100

    Fetch info about nodes if available when creating them.

--- meister/aws/driver.py
+++ meister/aws/driver.py
@@ -5,11 +5,12 @@ Created on Jan 10, 2013
 '''
 
 import ec2
+import route53
 
 from provisioner import Provisioner
 from libcloud.compute.types import Provider
 
-class Driver:
+class EC2Driver:
     REGIONS = {
         "eu-west-1": Provider.EC2_EU_WEST,
         "us-west-1": Provider.EC2_US_WEST,
@@ -28,21 +29,27 @@ class Driver:
         self.defaultSecurityGroup = settings['driver']['defaultSecurityGroup']
         config.getSecurityGroups = self.getSecurityGroups
         self.config = config
+        self.con = None
         if 'securityGroups' in settings.keys():
             self.securityGroups = settings['securityGroups']
     
     def getConnection(self):
-        return ec2.EC2Connection(self.aws_region, self.aws_id, self.aws_key)
-
+        if not self.con:
+            self.con = ec2.EC2Connection(self.aws_region, self.aws_id, self.aws_key)
+        return self.con
 
     def getSecurityGroups(self):
         return self.securityGroups
 
-
     def getNode(self, name, definition):
+        con = self.getConnection()
+        nodes = con.getNodes()
         for prop, defaultProp in {"securityGroup": "defaultSecurityGroup", "zone": "defaultZone"}.items():
             if not prop in definition:
                 definition[prop] = getattr(self, defaultProp)
+        if name in nodes:
+            definition["internalIp"] = nodes[name].private_ip[0] if len(nodes[name].private_ip) else None 
+            definition["externalIp"] = nodes[name].public_ip[0] if len(nodes[name].public_ip) else None
         return AWSNode(name, definition)
 
     def provision(self, logger):
@@ -51,7 +58,37 @@ class Driver:
         """
         provisioner = Provisioner(self.getConnection(), logger)
         provisioner.provisionSecurityGroups(self.getSecurityGroups())
-        provisioner.provisionNodes(self.config.getNodes())
+        nodes = provisioner.provisionNodes(self.config.getNodes())
+
+
+class Route53Driver:
+    def __init__(self, config, settings):
+        self.aws_id = settings['id']
+        self.aws_key = settings['key']
+        self.defaultZone = settings['defaultZone']
+        self.config = config 
+    
+    def getConnection(self):
+        return route53.Route53Connection(self.aws_id, self.aws_id)
+    
+    def provision(self, nodes, logger):
+        con = self.getConnection()
+        zones = con.getZones()
+        if not self.defaultZone in zones:
+            zone = con.saveZone(self.defaultZone)
+        else:
+            zone = con.getZone(zones[self.defaultZone].id)
+        for node in nodes:
+            for ipProp,nameProp in [("internalIp", "internalDNS"), ("externalIp", "externalDNS")]:
+                if hasattr(node, ipProp) and hasattr(node, nameProp):
+                    ip =  getattr(node, ipProp)
+                    name = getattr(node, nameProp)
+                    record = zone.getRecord(name)
+                    if not record:
+                        zone.addRecord("A", name, ip)
+                    elif ip not in record["value"]:
+                        record.append(ip)
+                        zone.updateRecord(name, record)
 
 class AWSNode():
     def __init__(self, name, definition):
@@ -60,7 +97,7 @@ class AWSNode():
         }
         self.name = name
         self.hostname = definition['hostname']
-        for prop in ['image', 'securityGroup', 'size', 'diskSize', 'zone', "externalDNS", "internalDNS"]:
+        for prop in ['image', 'securityGroup', 'size', 'diskSize', 'zone', "externalDNS", "internalDNS", "internalIp", "externalIp"]:
             if prop in definition:
                 setattr(self, prop, definition[prop])
             elif prop in defaults:
