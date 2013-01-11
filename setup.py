47fc698cce3b07c32642980f91f1209df6e95b7a
Fabian Sorqvist <fabian@sorqvist.nu>
2013-01-11 18:04:57 +0100

    Setup script

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(name='Meister',
+      version='0.1',
+      description='Keep track of complex cloud setups.',
+      author='Fabian Sörqvist',
+      author_email='fabian.sorqvist@wunderkraut.com',
+      url='https://github.com/WKLive/meister',
+      packages=find_packages(),
+      install_requires=['fabric>=1.0', "apache-libcloud>=0.10"],
+      tests_require=["nose"],
+      entry_points={
+        'console_scripts': [
+            'meister = meister.main:main',
+        ]
+      },
+ )
+
