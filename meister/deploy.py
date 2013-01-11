8ad1dce6288dd5641bf2e706eaacebd8d7e86305
Fabian Sorqvist <fabian@sorqvist.nu>
2013-01-11 12:12:30 +0100

    Deployer class to execute scripts

---
+++ meister/deploy.py
@@ -0,0 +1,31 @@
+'''
+Created on Jan 11, 2013
+
+@author: fabsor
+'''
+from fabric.api import settings, abort, run, cd, sudo, put, env, prompt, get
+
+class Deployer:
+    
+    def __init__(self, hostname, port = None, username = None, keyFile = None):
+        self.hostname = hostname
+        self.port = port
+        self.username = username
+        self.keyFile = keyFile
+        self.hoststring = hostname
+        if port:
+            self.hoststring = "{0}:{1}".format(self.hoststring, port)
+        if username:
+            self.hoststring = "{0}@{1}".format(username, self.hoststring)
+
+    def put(self, localPath, remotePath = None, useSudo = False):
+        with settings(host_string=self.hoststring, key_filename=self.keyFile):
+            put(localPath, remotePath, useSudo)
+    
+    def run(self, command):
+        with settings(host_string=self.hoststring, key_filename=self.keyFile):
+            run(command)
+        
+    def sudo(self, command):
+        with settings(host_string=self.hoststring, key_filename=self.keyFile):
+            sudo(command)
---
+++ meister/test/deployertest.py
@@ -0,0 +1,34 @@
+'''
+Created on Jan 11, 2013
+
+@author: fabsor
+'''
+
+import unittest
+import secrets
+import time
+from meister.aws import ec2
+from meister.deploy import Deployer
+from libcloud.compute.types import Provider
+
+
+class DeployerTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.con = ec2.EC2Connection(Provider.EC2_EU_WEST, secrets.EC2_ACCESS_ID, secrets.EC2_SECRET_KEY)
+        self.node = self.con.createNode('ami-c1aaabb5', 't1.micro', 'testnode', keyName="example")
+        # Wait for running state.
+        while self.node.extra["status"] != "running":
+            nodes = self.con.getNodes(True)
+            self.node = nodes['testnode']
+            time.sleep(10)
+        
+    def testDeploy(self):
+        deployer = Deployer(self.node.public_ip[0], username="ubuntu", keyFile=secrets.KEY_FILE)
+        deployer.run("ls /")
+        deployer.sudo("ls /")
+        deployer.put("testfile", "/home/ubuntu/testfile")
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+        self.node.destroy()
